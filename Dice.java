65f8118c2e7ba9d5af8359fc7ae5bf641eed8d67
Logan <Logan@DESKTOP-BV61DS4>
2020-05-25 19:53:28 -0400

    dice

--- Dice.java
+++ Dice.java
@@ -1,18 +1,15 @@
-
+/**
+ * 
+ * @author Ryan Greiser & Logan Davison
+ *
+ */
 public class Dice 
 {
-	public static void main (String[] args)
-	{
-		for(int i = 0; i < 10; i++)
-		{
-			System.out.println(roll());
-		}
-	}
+	
 	public static int roll()
 	{
 		return (int)(Math.random()*6 + 1);	
 	}
-	
 	
 	
 }
--- EelDice.java
+++ EelDice.java
@@ -1,8 +1,13 @@
-
+/**
+ * 
+ * @author Ryan Greiser & Logan Davison
+ *
+ */
 public class EelDice 
 {
 
-	
+	//true -> esclator
+	//false -> eel
 	public static boolean roll()
 	{
 		int num = (int)(Math.random()*2);
